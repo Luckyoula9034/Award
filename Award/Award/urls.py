59eb721c168d6f9960fd67a386fc300255a7dbc6
luqqy <you@example.com>
2020-10-26 18:30:18 +0300

    deployment to heroku

--- Award/Award/urls.py
+++ Award/Award/urls.py
@@ -14,19 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import url,include
+from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('',include('project.urls')),
-    url('accounts/', include('django.contrib.auth.urls')),
-    url('accounts/', include('django_registration.backends.one_step.urls')),
-    url('logout/',auth_views.LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL)),
-    url('tinymce/', include('tinymce.urls')),
-    url('ratings/', include('star_ratings.urls', namespace='ratings')),
+    path('admin/', admin.site.urls),
+    path('',include('project.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('logout/',auth_views.LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL)),
+    path('tinymce/', include('tinymce.urls')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
 ]
 
 
