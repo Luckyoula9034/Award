658ff95faad1d7529bde50fb83cd8ddb62e7027f
luqqy <you@example.com>
2020-10-26 08:20:41 +0300

    made changes in search file

--- Award/Award/settings.py
+++ Award/Award/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Award.apps.AwardConfig',
+    'project.apps.ProjectConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project'
+    
 ]
 
 MIDDLEWARE = [
