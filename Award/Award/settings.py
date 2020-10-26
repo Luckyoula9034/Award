648181a48c7f6c1bb6a33176ac75b7ce498d2140
luqqy <you@example.com>
2020-10-26 14:18:41 +0300

    deployment to heroku

--- Award/Award/settings.py
+++ Award/Award/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project.apps.ProjectConfig',
+    'Award.apps.AwardConfig',
     'tinymce',
     'django_registration',
     'bootstrap4',
