74c07c2920380c67c92efe5a46abfb5f47ee6f34
luqqy <you@example.com>
2020-10-26 18:52:20 +0300

    deployment to heroku

--- Award/project/models.py
+++ Award/project/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from jsonfield import JSONField
+
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericRelation
 from django.urls import reverse
@@ -90,5 +90,4 @@ class Review(models.Model):
         return total
 
     
-class MyModel(models.Model):
-    json = JSONField()    
+ 
