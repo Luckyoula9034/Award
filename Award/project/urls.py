1e300a85e73a049170ece5b148e3b6d7a8f8b4e3
luqqy <you@example.com>
2020-10-26 12:30:42 +0300

    updated the app

--- Award/project/urls.py
+++ Award/project/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views 
 
-
-urlpattterns=[
-    path('^$',views.home,name='home'),
+urlpatterns = [
+    path('',views.home,name='home'),
     path('profile/<int:id>/',views.profile,name='profile'), 
+    path('',views.review_form,name='review' ),
+    path('search/', views.search_results, name='search_results') 
+    
+    
+    
+
+    
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- Award/project/views.py
+++ Award/project/views.py
@@ -1,8 +1,60 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import render,reverse,redirect
+from django.http import HttpResponse
+from .models import Profile,Project,User,Review
+from .forms import ProfileForm,ProjectForm,ReviewForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Welcome to the Award app')
-def profile(request):
-    return HttpResponse('profile.html')    
+
+@login_required(login_url='/accounts/login')
+def home(request): 
+    posts=Project.objects.all()
+    if request.method == 'POST':
+        form= ProjectForm(request.POST,request.FILES)
+        rate = ReviewForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+    else:
+        form=ProjectForm()
+    return render(request,"home.html",{"posts":posts, "form":form})
+
+
+def profile(request,id):
+    user = User.objects.get(id=id)
+    profile = Profile.objects.filter(user = request.user) 
+    posts = Project.objects.filter(author__id=id)[::-1]
+    p_form= ProfileForm()
+    return render(request, "profile.html", context={"user":user,
+                                                             "profile":profile,
+                                                             "posts":posts,"p_form":p_form})
+    
+
+
+def review_form(request):
+    current_user=request.user
+    if request.method =="POST":
+        rate = ReviewForm(request.POST)
+        if rate.is_valid():
+            review=rate.save(commit=False)
+            review.author=current_user
+            review.save()
+            
+            return redirect('home')
+    else:
+        rate = ReviewForm()
+    return render(request,"home.html",{"rate":rate,"current_user":current_user})
+    
+
+def search_results(request):
+
+    if 'search' in request.GET and request.GET["search"]:
+        search_term = request.GET.get("search")
+        searched_articles = Project.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"projects": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
